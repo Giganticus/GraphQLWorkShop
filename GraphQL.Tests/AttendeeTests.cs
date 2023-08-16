0dddc58ce4ac4cfadd768d51d65a400ad881fe20
Christopher Booth <christopher.booth@juliusbaer.com>
2023-08-16 17:35:13 +0200

    Adding a test for a mutation (RegisterAttendee)

--- GraphQL.Tests/AttendeeTests.cs
+++ GraphQL.Tests/AttendeeTests.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using ConferencePlanner.GraphQL.Attendees;
 using ConferencePlanner.GraphQL.Data;
 using ConferencePlanner.GraphQL.Types;
+using HotChocolate;
 using HotChocolate.Data;
 using HotChocolate.Execution;
 using Microsoft.EntityFrameworkCore;
@@ -38,5 +39,47 @@ public class AttendeeTests
         // assert
         var schemaPrint = schema.Print();
         schema.Print().MatchSnapshot();
+    }
+    
+    [Fact]
+    public async Task RegisterAttendee()
+    {
+        // arrange
+        IRequestExecutor executor = await new ServiceCollection()
+            .AddPooledDbContextFactory<ApplicationDbContext>(
+                options => options.UseInMemoryDatabase("Data Source=conferences.db"))
+            .AddGraphQL()
+            .AddQueryType(d => d.Name("Query"))
+            .AddTypeExtension<AttendeeQueries>()
+            .AddMutationType(d => d.Name("Mutation"))
+            .AddTypeExtension<AttendeeMutations>()
+            .AddType<AttendeeType>()
+            .AddType<SessionType>()
+            .AddType<SpeakerType>()
+            .AddType<TrackType>()
+            .AddGlobalObjectIdentification()
+            .RegisterDbContext<ApplicationDbContext>(DbContextKind.Pooled)
+            .BuildRequestExecutorAsync();
+
+        // act
+        IExecutionResult result = await executor.ExecuteAsync(@"
+        mutation RegisterAttendee {
+            registerAttendee(
+                input: {
+                    emailAddress: ""michael@chillicream.com""
+                        firstName: ""michael""
+                        lastName: ""staib""
+                        userName: ""michael3""
+                    })
+            {
+                attendee {
+                    id
+                }
+            }
+        }");
+
+        // assert
+        var resultString = result.ToJson();
+        result.ToJson().MatchSnapshot();
     }
 }
